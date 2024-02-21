9396c9cdf12337238cbb63218c4e49c3f4420814
fResult Sila <Styxmaz@gmail.com>
2024-02-22 00:45:07 +0700

    implement `replace_char_in_clue_chars` function

--- game-1.py
+++ game-1.py
@@ -1,13 +1,15 @@
 from random import choice
-from typing import Callable, List, TypeVar
+from typing import Callable, List, TypeAlias, TypeVar
 
 T = TypeVar('T')
+R = TypeVar('R')
 PredicateFn = Callable[[T], bool]
+MapperFn = Callable[[T], R]
 
 INITIAL_GAME_STATE = {
   "score": 0,
   "lives": 3,
-  "words": ['great', 'cherprang', 'jaa', 'korn', 'jennie'],
+  "names": ['great', 'cherprang', 'jaa', 'korn', 'jennie'],
 }
 
 def is_alive(lives: int) -> bool:
@@ -20,8 +22,12 @@ def is_same_name(word: str):
   for_name: Callable[[str], bool] = lambda name: name == word
   return for_name
 
-def filter_by(predicateFn: PredicateFn, xs):
+def filter_by(predicateFn: PredicateFn, xs: List[T]) -> List[T]:
   return list(filter(predicateFn, xs))
+
+def map_by(f: MapperFn, xs: List[T]) -> List[R]:
+  return list(map(f, xs))
+
 
 def pick_name_from_names(names: List[str]) -> str:
   return choice(names)
@@ -32,25 +38,59 @@ def negate(predicate_fn: PredicateFn) -> PredicateFn:
 def create_clue_characters(word: str) -> str:
   return list(len(word) * '?')
 
-# def replace_char_in_clue_chars(char: chr) -> List[chr]:
+def find_index(char: chr, chars: List[chr]) -> int:
+  try:
+    return chars.index(char)
+  except ValueError:
+    return -1
 
-remainingWords = INITIAL_GAME_STATE['words']
+def replace_char_in_clue_chars(char: chr, name: str, clue: List[chr]) -> List[chr]:
+  found_idx: int = -1
+  name_chars = list(name)
+  updated_clue = clue.copy()
+  print(name_chars)
+
+  while find_index(char, name_chars) >= 0:
+    found_idx = name_chars.index(char)
+    if found_idx >= 0:
+      updated_clue = [name[found_idx] if idx == found_idx else c for idx, c in enumerate(updated_clue)]
+      name_chars = ['' if idx == found_idx else c for idx, c in enumerate(name_chars)]
+      print('chars:', name_chars)
+    else:
+      updated_clue = clue
+
+
+  return updated_clue
+
+
+remainingWords = INITIAL_GAME_STATE['names']
 picked_name = pick_name_from_names(remainingWords)
 remainingWords = filter_by(negate(is_same_name(picked_name)), remainingWords)
 
-print(f'clue word of {picked_name} is: {create_clue_characters(picked_name)}')
-print('picked name:', picked_name)
-print('remaining name:', remainingWords)
-print('remain?:', is_remain(remainingWords))
+# print(f'clue word of {picked_name} is: {create_clue_characters(picked_name)}')
+# print('picked name:', picked_name)
+# print('remaining name:', remainingWords)
+# print('remain?:', is_remain(remainingWords))
 
-picked_name = pick_name_from_names(remainingWords)
-remainingWords = filter_by(negate(is_same_name(picked_name)), remainingWords)
-print('picked name:', picked_name)
-print('remaining name:', remainingWords)
-print('remain?:', is_remain(remainingWords))
+# picked_name = pick_name_from_names(remainingWords)
+# remainingWords = filter_by(negate(is_same_name(picked_name)), remainingWords)
+# print('picked name:', picked_name)
+# print('remaining name:', remainingWords)
+# print('remain?:', is_remain(remainingWords))
 
-picked_name = pick_name_from_names(remainingWords)
-remainingWords = filter_by(negate(is_same_name(picked_name)), remainingWords)
-print('picked name:', picked_name)
-print('remaining name:', remainingWords)
-print('remain?:', is_remain(remainingWords))
+# picked_name = pick_name_from_names(remainingWords)
+# remainingWords = filter_by(negate(is_same_name(picked_name)), remainingWords)
+# print('picked name:', picked_name)
+# print('remaining name:', remainingWords)
+# print('remain?:', is_remain(remainingWords))
+
+def main():
+  remaining_names = INITIAL_GAME_STATE['names']
+  picked_name = pick_name_from_names(remaining_names)
+  print(f'Picked Name is: {picked_name}')
+  clue_chars = create_clue_characters(picked_name)
+  guessed_char = input('Please guess character in a name: ')
+  replaced_chars = replace_char_in_clue_chars(guessed_char, picked_name, clue_chars)
+  print(replaced_chars)
+
+main()
