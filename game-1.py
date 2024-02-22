8ea9b33e4467c41a35533bb9282066c43b618af4
fResult Sila <Styxmaz@gmail.com>
2024-02-22 13:08:14 +0700

    Refactor some functions

--- game-1.py
+++ game-1.py
@@ -1,5 +1,5 @@
 from random import choice
-from typing import Callable, List, TypeAlias, TypeVar
+from typing import Callable, List, TypeVar
 
 T = TypeVar('T')
 R = TypeVar('R')
@@ -18,16 +18,23 @@ def is_alive(lives: int) -> bool:
 def is_remain(items: List[T]) -> int:
   return len(items) > 0
 
-def is_same_name(word: str):
-  for_name: Callable[[str], bool] = lambda name: name == word
-  return for_name
+def is_guess_all(guessed_name: str, clue_characters: List[str]) -> bool:
+  clue_word = ''.join(clue_characters)
+  return guessed_name == clue_word
 
-def filter_by(predicateFn: PredicateFn, xs: List[T]) -> List[T]:
-  return list(filter(predicateFn, xs))
+def is_equal(y: T):
+  for_x: Callable[[T], bool] = lambda x: x == y
+  return for_x
 
-def map_by(f: MapperFn, xs: List[T]) -> List[R]:
-  return list(map(f, xs))
+def filter_by(predicate: PredicateFn):
+  def for_xs(xs: List[T]) -> List[T]:
+    return [x for x in xs if predicate(x)]
+  return for_xs
 
+def map_by(f: MapperFn):
+  def for_xs(xs: List[T]) -> List[R]:
+    return [f(x) for x in xs]
+  return for_xs
 
 def pick_name_from_names(names: List[str]) -> str:
   return choice(names)
@@ -38,33 +45,14 @@ def negate(predicate_fn: PredicateFn) -> PredicateFn:
 def create_clue_characters(word: str) -> str:
   return list(len(word) * '?')
 
-def find_index(char: chr, chars: List[chr]) -> int:
-  try:
-    return chars.index(char)
-  except ValueError:
-    return -1
-
 def replace_char_in_clue_chars(guessed_char: chr, name: str, clue: List[chr]) -> List[chr]:
-  found_idx: int = -1
   name_chars = list(name)
-  # updated_clue = clue.copy()
-
-  # while find_index(char, name_chars) >= 0:
-  #   found_idx = name_chars.index(char)
-  #   if found_idx >= 0:
-  #     updated_clue = [name[found_idx] if idx == found_idx else c for idx, c in enumerate(updated_clue)]
-  #     name_chars = ['' if idx == found_idx else c for idx, c in enumerate(name_chars)]
-  #   else:
-  #     updated_clue = clue
-
-  updated_clue = [c if c == guessed_char else '?' for c in name_chars]
-
-  return updated_clue
+  return [ch if ch == guessed_char else '?' for ch in name_chars]
 
 
 remainingWords = INITIAL_GAME_STATE['names']
 picked_name = pick_name_from_names(remainingWords)
-remainingWords = filter_by(negate(is_same_name(picked_name)), remainingWords)
+remainingWords = filter_by(negate(is_equal(picked_name)))(remainingWords)
 
 # print(f'clue word of {picked_name} is: {create_clue_characters(picked_name)}')
 # print('picked name:', picked_name)
@@ -72,13 +60,13 @@ remainingWords = filter_by(negate(is_same_name(picked_name)), remainingWords)
 # print('remain?:', is_remain(remainingWords))
 
 # picked_name = pick_name_from_names(remainingWords)
-# remainingWords = filter_by(negate(is_same_name(picked_name)), remainingWords)
+# remainingWords = filter_by(negate(is_equal(picked_name)))(remainingWords)
 # print('picked name:', picked_name)
 # print('remaining name:', remainingWords)
 # print('remain?:', is_remain(remainingWords))
 
 # picked_name = pick_name_from_names(remainingWords)
-# remainingWords = filter_by(negate(is_same_name(picked_name)), remainingWords)
+# remainingWords = filter_by(negate(is_equal(picked_name)))(remainingWords)
 # print('picked name:', picked_name)
 # print('remaining name:', remainingWords)
 # print('remain?:', is_remain(remainingWords))
@@ -90,6 +78,6 @@ def main():
   clue_chars = create_clue_characters(picked_name)
   guessed_char = input('Please guess character in a name: ')
   replaced_chars = replace_char_in_clue_chars(guessed_char, picked_name, clue_chars)
-  print(replaced_chars)
+  print('replaced chars:', replaced_chars)
 
-main()
+# main()
